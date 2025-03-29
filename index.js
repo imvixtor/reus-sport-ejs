010bb8e0d884bf303c7f6b795979637b65422546
khanhptit202 <khanhptit202@gmail.com>
2025-03-29 18:42:49 +0700

    tôi thêm comment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
-
+// Khánh đây
 app.use(session({
     secret: 'super-secret-key',
     resave: false,
