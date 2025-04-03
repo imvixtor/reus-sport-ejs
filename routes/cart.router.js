c6c649135ed403d9b409371ac6cb3b2c5fcd3e8e
imvixtor <quanlb.b21cn607@stu.ptit.edu.vn>
2025-04-03 15:31:12 +0700

    thêm các trang ejs: giỏ hàng, thanh toán, hoàn tất sửa đường dẫn ảnh khi hiển thị

--- routes/cart.router.js
+++ routes/cart.router.js
@@ -3,7 +3,15 @@ import requireAuth from '../middlewares/requireAuth.middleware.js';
 const cartRouter = express.Router();
 
 cartRouter.get('/cart', requireAuth, async (req, res) => {
-    return res.redirect('/404');
+    return res.render('cart', { title: 'Giỏ hàng - Reus sport', user: req.session.user });
+});
+
+cartRouter.get('/checkout', requireAuth, async (req, res) => {
+    return res.render('checkout', { title: 'Đặt hàng - Reus sport', user: req.session.user });
+});
+
+cartRouter.get('/checkout/complete', requireAuth, async (req, res) => {
+    return res.render('checkout-complete', { title: 'Đặt hàng thành công - Reus sport', user: req.session.user });
 });
 
 export default cartRouter;
